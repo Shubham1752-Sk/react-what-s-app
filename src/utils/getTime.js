9d4aa36c5026a0a96dcd012508f5d5b2a8767339
Shubham1752-Sk <shubhamsk1752@gmail.com>
2024-02-01 18:47:15 +0530

    removed cache

--- src/utils/getTime.js
+++
@@ -1,6 +0,0 @@
-export const getTime = (date) => {
-    const d = new Date(date).toLocaleTimeString().split(" ")
-    const hr = d[0].split(":")[0]
-    const min = d[0].split(":")[1]
-    return (`${hr}:${min} ${d[1]}`)
-}
